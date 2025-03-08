55e19d172de4af1cad5ee009faa46075e91a5331
fisal-new <144497595+fisal-new@users.noreply.github.com>
2025-03-08 13:06:29 +0300

    Update Drakthon.lua

--- Drakthon.lua
+++ Drakthon.lua
@@ -1,5 +1,5 @@
 local PlaceId = game.PlaceId  -- الحصول على الـ PlaceId الحالي
---ماني مسامح اي شخص يسرق تعبي جعله للغرغرينا تقول اساسا السكربت حرام يعني ماهي فارقه اقلك حتا لو يهودي حرامم
+
 -- تحميل Rayfield UI
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()  
 local Window = Rayfield:CreateWindow({
@@ -16,6 +16,7 @@ local MiscTab = Window:CreateTab("Misc")
 local FunTab = Window:CreateTab("Fun")
 local TimeTab = Window:CreateTab("Time Settings")
 local MusicTab = Window:CreateTab("Music")
+local SocialTab = Window:CreateTab("Social")
 
 -- تبويبات خاصة حسب الماب
 if PlaceId == 2753915549 then
@@ -45,9 +46,16 @@ elseif PlaceId == 9872472334 then
             loadstring(game:HttpGet("https://rawscripts.net/raw/Evade-Evade-The-Best-Free-GUI-Script-lots-of-Features-20718"))()
         end
     })
+elseif PlaceId == 115110570222234 then
+    -- إذا كان الماب Blue Lock
+    local BlueLockTab = Window:CreateTab("Blue Lock")
+    BlueLockTab:CreateButton({
+        Name = "Load Blue Lock Script",
+        Callback = function()
+            loadstring(game:HttpGet("https://rawscripts.net/raw/UPD-Blue-Lock:-Rivals-Rat-Hub-7-DON-LORENZO-FREE-STYLE-CHANGER-AND-FLOW-NO-KEY-7-30715"))()
+        end
+    })
 end
-
--- باقي السكربتات العامة:
 
 -- تبويب Player Settings
 PlayerTab:CreateSlider({
@@ -232,5 +240,23 @@ PlayerTab:CreateButton({
     Name = "Enable Dex",
     Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/DexV2/DexV2/main/DexV2.lua"))()
+    end
+})
+
+-- إضافة رابط دعوة سيرفر Discord
+SocialTab:CreateButton({
+    Name = "Join Our Discord",
+    Callback = function()
+        setclipboard("https://discord.gg/W3VwkUj6kf")  -- رابط الدعوة الجديد
+        print("تم نسخ رابط الدعوة إلى الحافظة!")
+    end
+})
+
+-- إضافة زر نسخ ID الماب في تبويب Music
+MusicTab:CreateButton({
+    Name = "Copy ID Map",
+    Callback = function()
+        setclipboard(tostring(PlaceId))  -- نسخ الـ ID الخاص بالماب
+        print("تم نسخ ID الماب إلى الحافظة!")
     end
 })

ae5668d38efb0ac397aaff85a197ad9deab4413d
fisal-new <144497595+fisal-new@users.noreply.github.com>
2025-03-12 02:03:13 +0300

    Update Testlolidk.lua

--- Testlolidk.lua
+++ Testlolidk.lua
@@ -149,6 +149,15 @@ elseif PlaceId == 6961824067 then
          end 
     })
 end
+    })
+elseif PlaceId == 103593441753340 or PlaceId == 82775216869079 then
+    local findthebuttonTab = Window:CreateTab("find the button [150]")
+    findthebuttonTab:CreateButton({
+        Name = "Auto finish gui",
+        Callback = function()
+            loadstring(game:HttpGet("https://rawscripts.net/raw/Find-The-Button!-esp-button-and-auto-finish-lv-27417"))()
+        end
+    })
 -- باقي السكربتات العامة:
 
 -- تبويب Player Settings
